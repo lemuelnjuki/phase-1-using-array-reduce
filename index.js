489fc160ebcfcadece44ad53c360acfeed2f40b2
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-21 08:58:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-//const totalBatteries =batteryBatches.reduce((assembled,batteries)=> assembled =batteries,0);
 const totalBatteries = batteryBatches.reduce((x,y) => {return x +y})
 
 
